d1165c5bf5161e1ad8bdb32f61dc7a66f332edf9
효정이 <w2029@e-mirim.hs.kr>
2021-11-26 16:03:55 +0900

    Login_Frame 창 만들기

---
+++ replanet/src/replanet/Login_Frame.java
@@ -0,0 +1,20 @@
+package replanet;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+import com.sun.tools.javac.Main;
+
+public class Login_Frame extends JFrame{
+	public Login_Frame() {
+		setTitle("replanet");
+		setSize(1000, 700);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+}
