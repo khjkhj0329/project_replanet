5c9e92832e4004ad4af96b4781187a074ceefacf
user <w2005@e-mirim.hs.kr>
2021-11-27 12:12:59 +0900

    테스트

--- replanet/src/replanet/test.java
+++ replanet/src/replanet/test.java
@@ -3,7 +3,9 @@ package replanet;
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stu
+		
+		System.out.println("Å×½ºÆ® Ä¿¹Ô");
 
 	}
 
