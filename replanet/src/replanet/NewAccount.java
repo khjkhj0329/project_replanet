4190d9fbe3511af33640b987828362b451b32586
user <w2005@e-mirim.hs.kr>
2021-11-27 12:10:28 +0900

    test

---
+++ replanet/src/replanet/NewAccount.java
@@ -0,0 +1,50 @@
+package replanet;
+
+import java.awt.Image;
+
+import javax.swing.*;
+
+class Account{
+	private String id;
+	private String password;
+	private String name;
+	private String email;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
+
+public class NewAccount {
+	public static void main(String[] args) {
+		private Panel panel = new Panel();
+		
+	}
+	class Panel extends JPanel{
+		private ImageIcon icon = new ImageIcon("C:\\Users\\user\\Documents\\Piskel-0.14.0\\source\\space.png");
+		private Image img= icon.getImage();
+		
+	}
+}
